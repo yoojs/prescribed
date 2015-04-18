4dda622c743179a38967f6d8e6a2f296f5646383
yoojs <yoojs@dukes.jmu.edu>
2015-04-17 22:43:09 -0400

    made server and pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get 'pages/doctor'
 
   get 'pages/patient'
+  root 'pages#login'
+
+  get 'doctor' => 'pages#doctor'
+
+  get 'patient' => 'pages#patient'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
